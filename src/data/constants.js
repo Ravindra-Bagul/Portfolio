2fc82a036aff82c273c6ff16199a7fec86fb0d5f
Ravi <132358935+Ravindra-Bagul@users.noreply.github.com>
2025-01-21 20:21:47 +0530

    update

---
+++ src/data/constants.js
@@ -0,0 +1,356 @@
+export const Bio = {
+  name: "Ravindra Bagul",
+  roles: [
+    "Full Stack Developer",
+    "Developer",
+    "Programmer",
+    "Innovator",
+    
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/Ravindra-Bagul",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  linkedin: "http://www.linkedin.com/in/ravindra-bagul",
+  email: "ravindradayarambagul@gmail.com",
+  // insta: "https://www.instagram.com/rishav_chanda/",
+  // facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    description: "Strong foundation in multiple programming languages. I excel in writing clean, efficient, and maintainable code tailored to various needs and applications.\n",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://cdn3d.iconscout.com/3d/free/thumb/free-java-3d-icon-download-in-png-blend-fbx-gltf-file-formats--object-oriented-jvm-logo-applications-coding-lang-pack-logos-icons-7578017.png",
+      },
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1822px-ISO_C%2B%2B_Logo.svg.png",
+      },
+      {
+        name: "Python",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/1024px-Python.svg.png",
+      },
+     
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+     
+    ],
+  },
+  {
+    title: "Database Management",
+    description: "Efficient and Scalable Data Handling Expertise. I specialize in managing, designing, and optimizing databases to ensure seamless data storage, retrieval, and security",
+    skills: [
+      
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+     
+    ],
+  },
+  {
+    title: "Frontend Development",
+    description: "Creating Intuitive and Responsive User Interfaces. I specialize in designing and implementing visually appealing, user-friendly web interfaces that deliver exceptional user experiences",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2048px-Git_icon.svg.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "NetBeans",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Apache_NetBeans_Logo.svg/1200px-Apache_NetBeans_Logo.svg.png",
+      },
+      {
+        name: "SQL Workbench",
+        image:
+          "https://cdn.icon-icons.com/icons2/1381/PNG/512/mysqlworkbench_93532.png",
+      },
+      {
+        name: " Microsoft Office",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/4/49/Office_2021.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://images.credly.com/size/680x680/images/be8fcaeb-c769-4858-b567-ffaaa73ce8cf/image.png",
+    name: "Microsoft Certified: Azure Fundamentals",
+    company: "Microsoft",
+    date: "February 04, 2024",
+    desc: "Gained foundational understanding of cloud concepts, including public, private, and hybrid cloud models, as well as IaaS, PaaS, and SaaS. Acquired knowledge of Azure architectural components, services, and management tools. Successfully passed Exam AZ-900: Microsoft Azure Fundamentals, demonstrating competence in Azure fundamentals.",
+    skills: [
+      "Azure",
+      "Cloud Data",
+      "Cloud Networking",
+      "Cloud Security",
+      "Cloud Services",
+      "Cloud Storage",
+      "Virtualization",
+    ],
+    link: "https://www.credly.com/badges/f4e9048e-728b-4436-bae4-0f79e6a49faa",
+  },
+  {
+    id: 1,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/HackerRank_Icon-1000px.png/800px-HackerRank_Icon-1000px.png",
+    name: "Java Skills Certification",
+    company: "HackerRank",
+    date: "February 12, 2024",
+    desc: "Demonstrated proficiency in Java programming by successfully completing the HackerRank Java (Basic) Skills Certification Test. Showcased knowledge in core Java concepts including classes, data structures, inheritance, and exception handling. Achieved HackerRank verified developer status, reflecting a strong foundation in Java 7 and Java 8.",
+    skills: [
+      "Java",
+      "Object-Oriented Programming",
+      "Data Structures",
+      "Problem Solving",
+      "Algorithms",
+      "Exception Handling",  
+    ],
+    link: "https://www.hackerrank.com/certificates/89e1031a39b3",
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/HackerRank_Icon-1000px.png/800px-HackerRank_Icon-1000px.png",
+    name: "SQL Skills Certification",
+    company: "HackerRank",
+    date: "February 17, 2024",
+    desc: "Acquired advanced SQL skills, including data manipulation with aggregations and string functions. Mastered complex data extraction from multi-table databases and performed join queries to merge data from interrelated tables. Completed coursework to enhance query capabilities and understanding of relational database management systems.",
+    skills: [
+      "SQL", 
+      "Problem Solving", 
+      "Advanced Data Manipulation",
+      "Normalization", 
+      "Subqueries & Nested Queries",
+      "Complex Joins",
+    ],
+    link: "https://www.hackerrank.com/certificates/c9048f8126aa",
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAAiPkyk9wjoPdFW1WXya1UCFsFNw0ZYBDew&s",
+    name: "Complete Introduction to C++ Programming",
+    company: "Udemy",
+    date: "August 23, 2023",
+    desc: "Gained a deep understanding of complex C++ functions and their applications. Acquired advanced skills in C++ through practical tips and tricks, developing the ability to create basic C++ programs independently. Progressed at a personalized pace with hands-on exercises in each session.",
+    skills: [
+      "C++",
+      "Object-Oriented Programming",
+      "Data Structures",
+      "Problem Solving",
+      "Algorithms",
+      "Exception Handling", 
+    ],
+    link: "https://www.udemy.com/certificate/UC-b4068353-fde9-4f08-ad4b-0329611022aa/",
+  },
+ 
+  // {
+  //   id: 1,
+  //   img: "",
+  //   name: "  ",
+  //   company: "",
+  //   date: "",
+  //   desc: "",
+  //   skills: [
+      
+  //   ],
+  //   link: "",
+  // },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1rFvVOP-j3cGX-ynBe_7oZcV9BX_LCkMg3w&s",
+    school: "Smt. Kashibai Navale College of Engineering, Pune, India ",
+    date: "Dec 2021 - Jun 2025",
+    grade: "9.47 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Engineering at Smt. Kashibai Navale College of Engineering, Pune, India . I have completed 7 semesters and have a CGPA of 9.47. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a placement coordinator at my college, where I help facilitate and coordinate the placement activities for my peers.",
+    degree: "Bachelor of Engineering - BE, Computer Engineering",
+  },
+  {
+    id: 1,
+    img: "https://i.pinimg.com/originals/2b/ed/04/2bed0406182a8e0351d8fbc2f3458b7d.webp",
+    school: "K. A. A. N. M. Sonawane Arts, Science and Commerce College, Satana",
+    date: "Apr 2020 - Apr 2021",
+    grade: "89.17%",
+    desc: "Achieved 1st rank in the 12th grade in the Science stream, focusing on subjects like Mathematics, Physics, and Computer Science. This period helped solidify my foundation in scientific thinking, problem-solving, and the practical applications of technology, especially in the field of computer science. My academic success further fueled my interest in technology and engineering.",
+    degree: "HSC(XII), Science",
+  },
+  {
+    id: 2,
+    img: "https://img.freepik.com/premium-vector/school-logo-white-background_917213-255809.jpg",
+    school: "L. R. B. Patil Madhyamik Vidhyalay Juni Shemali, Satana",
+    date: "Apr 2018 - Apr 2019",
+    grade: "89.40%",
+    desc: "Achieved 1st rank in the 10th grade, excelling in subjects such as Science and Mathematics. My strong performance in high school set the foundation for my passion for technology and engineering, shaping my career path toward Computer Engineering. The skills I developed during this period helped me excel in both academic and extracurricular activities.",
+    degree: "SSC(X)",
+  },
+];
+
+export const projects = [
+
+  
+  {
+    id: 0,
+    title: "Advanced Voting System (Ongoing)",
+    date: "July 2024 - Apr 2025",
+    description:
+      "Designing a Python-based application for secure and efficient digital voting. Developing an advanced voting system with fingerprint and face recognition for secure voter authentication. Ensuring only registered voters can vote, preventing fraud and ensuring electoral integrity. Implementing the ability for voters to cast votes at nearby offline centers, regardless of location.",
+    image: "https://media.kasperskydaily.com/wp-content/uploads/sites/92/2020/10/16044143/M187_Digital-voting-header.png",
+    tags: ["Python", "Machine Learning", "OpenCV", "Face Recognition",],
+    category: "machine learning",
+    github: "",
+    webapp: "",
+    member: [
+      {
+        name: "Ravindra Bagul",
+        img: "https://w7.pngwing.com/pngs/219/345/png-transparent-computer-software-computer-icons-team-members-photography-team-logo.png",
+        linkedin: "",
+        github: "",
+      },
+      {
+        name: "Ankita Banakar",
+        img: "https://w7.pngwing.com/pngs/219/345/png-transparent-computer-software-computer-icons-team-members-photography-team-logo.png",
+        linkedin: "",
+        github: "",
+      },
+      {
+        name: "Abhishek Borse",
+        img: "https://w7.pngwing.com/pngs/219/345/png-transparent-computer-software-computer-icons-team-members-photography-team-logo.png",
+        linkedin: "",
+        github: "",
+      },
+      {
+        name: "Himanshu Lonkar",
+        img: "https://w7.pngwing.com/pngs/219/345/png-transparent-computer-software-computer-icons-team-members-photography-team-logo.png",
+        linkedin: "",
+        github: "",
+      },
+    ],
+  },
+
+  {
+    id: 1,
+    title: "ATM Management System",
+    date: "2023",
+    description:
+      "Developed an ATM simulator system replicating real-world ATM functionality with a user-friendly interface. Implemented features for opening banking accounts, allowing users to register and manage their details. Implemented core banking operations, including deposit, withdrawal and PIN change functionality. Utilized JDBC to integrate Java with a MySQL database, ensuring reliable data storage and retrieval.",
+    image:"https://miro.medium.com/v2/resize:fit:600/1*-YgB3rc_p-6luSd6epONoQ.jpeg",
+    tags: ["Java", "JDBC", "MySQL", "OOPs","SDLC",],
+    category: "desktop app",
+    github: "",
+    webapp: "",
+    member: [
+      {
+        name: "Ravindra Bagul",
+        img: "https://w7.pngwing.com/pngs/219/345/png-transparent-computer-software-computer-icons-team-members-photography-team-logo.png",
+        linkedin: "",
+        github: "",
+      },
+      {
+        name: "Ankita Banakar",
+        img: "https://w7.pngwing.com/pngs/219/345/png-transparent-computer-software-computer-icons-team-members-photography-team-logo.png",
+        linkedin: "",
+        github: "",
+      },
+     
+    ],
+  },
+
+  {
+    id: 2,
+    title: "Online Food Ordering System",
+    date: "2024",
+    description:
+      "Engineered a dynamic Online Food Ordering System using HTML, CSS, and JavaScript to facilitate seamless food ordering experiences. Designed a user-friendly interface with responsive web design techniques, ensuring compatibility across various devices and screen sizes. Implemented client-side scripting to create interactive features such as menu navigation, order customization, and real-time order updates.",
+    image: "https://foaps.co/wp-content/uploads/2022/06/What-is-Online-Food-Ordering-System-A-Guide-1.png",
+    tags: ["HTML", "CSS", "Javascript", "Web Development",],
+    category: "web app",
+    github: "",
+    webapp: "",
+    member: [
+      {
+        name: "Ravindra Bagul",
+        img: "https://w7.pngwing.com/pngs/219/345/png-transparent-computer-software-computer-icons-team-members-photography-team-logo.png",
+        linkedin: "",
+        github: "",
+      },
+      
+    ],
+  },
+  
+  // {
+  //   id: 5,
+  //   title: "Todo Web App",
+  //   date: "Jun 2021",
+  //   description:
+  //     " A Todo Web App made with React JS, Redux, and Material UI. It has a login page where users can log in with their Google account. It has a sidebar where users can see all the tasks and can create a new task. It has a calendar where users can see all the tasks on a particular date. It has a search bar where users can search for a particular task.",
+  //   image:
+  //     "https://camo.githubusercontent.com/a328255ad96f861f57d25096d28018ab2656c689a1456b0d145764009bed2d1a/68747470733a2f2f666972656261736573746f726167652e676f6f676c65617069732e636f6d2f76302f622f746f646f2d6170702d63386331392e61707073706f742e636f6d2f6f2f53637265656e73686f74253230283938292e706e673f616c743d6d6564696126746f6b656e3d33643335646366322d626666322d343730382d393031632d343232383866383332386633",
+  //   tags: ["React Js", "Local Storage", "AWS Auth", "Node JS"],
+  //   category: "web app",
+  //   github: "https://github.com/rishavchanda/Todo-Web-App",
+  //   webapp: "https://rishav-react-todo.netlify.app/",
+  // },
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
